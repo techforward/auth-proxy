e90481e62002b78da6595c9308f5adbd82fec861
ErgoFriend <erg.friend@gmail.com>
2019-11-30 15:39:15 +0900

    auth

--- auth/provider.go
+++ auth/provider.go
@@ -2,10 +2,10 @@ package auth
 
 import (
 	"fmt"
-	"math/rand"
+	"io/ioutil"
 	"time"
 
-	"github.com/oklog/ulid/v2"
+	"github.com/dgrijalva/jwt-go"
 )
 
 // Provider ...
@@ -18,8 +18,56 @@ func NewProvider(r Repository) *Provider {
 	return &Provider{r}
 }
 
-func (p *Provider) createToken() {
-	t := time.Unix(1000000, 0)
-	entropy := ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
-	fmt.Println(ulid.MustNew(ulid.Timestamp(t), entropy))
+func (p *Provider) create() string {
+	signBytes, err := ioutil.ReadFile("./demo.rsa")
+	if err != nil {
+		panic(err)
+	}
+
+	signKey, err := jwt.ParseRSAPrivateKeyFromPEM(signBytes)
+	if err != nil {
+		panic(err)
+	}
+
+	// create token
+	token := jwt.New(jwt.SigningMethodRS256)
+
+	// set claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["name"] = "test"
+	claims["admin"] = true
+	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+	tokenString, err := token.SignedString(signKey)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return tokenString
+}
+
+func (p *Provider) verify(tokenString string) bool {
+	verifyBytes, err := ioutil.ReadFile("./demo.rsa.pub.pkcs8")
+	if err != nil {
+		panic(err)
+	}
+	verifyKey, err := jwt.ParseRSAPublicKeyFromPEM(verifyBytes)
+	if err != nil {
+		panic(err)
+	}
+
+	// verify token
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		_, err := token.Method.(*jwt.SigningMethodRSA)
+		if !err {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		} else {
+			return verifyKey, nil
+		}
+	})
+
+	if err == nil && token.Valid {
+		return true
+	}
+	return false
 }
